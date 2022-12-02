23abd577a3512307fbc4d71a6e39478828d6296d
Luke Brobbin <luke.brobbin@xdesign.com>
2022-12-02 11:04:25 +0000

    chore: complete day 2

--- challenges/day-2/day-2.go
+++ challenges/day-2/day-2.go
@@ -1,5 +1,93 @@
 package day2
 
+import (
+	"strings"
+)
+
+type hand struct {
+	value    int
+	them     string
+	me       string
+	handType string
+}
+
+const ROCK = "rock"
+const PAPER = "paper"
+const SCISSORS = "scissors"
+
+var outcomes = map[string]string{
+	ROCK:     SCISSORS,
+	PAPER:    ROCK,
+	SCISSORS: PAPER,
+}
+
 func Day2(input string, part2 bool) int {
-	return 2
+	turns := strings.Split(input, "\n")
+
+	points := []hand{
+		{1, "A", "X", ROCK},
+		{2, "B", "Y", PAPER},
+		{3, "C", "Z", SCISSORS},
+	}
+	roundScore := 0
+	for _, turn := range turns {
+		t := strings.Split(turn, " ")
+		var them, me hand
+		for _, p := range points {
+			if p.them == t[0] {
+				them = p
+			}
+			if p.me == t[1] {
+				me = p
+			}
+		}
+		if part2 {
+			p := gamePointsPart2(t[1], them)
+			if p == 3 {
+				roundScore += them.value
+				roundScore += p
+				continue
+			}
+			for _, po := range points {
+				switch p {
+				case 6:
+					if outcomes[po.handType] == them.handType {
+						roundScore += po.value
+					}
+				default:
+					if po.handType == outcomes[them.handType] {
+						roundScore += po.value
+					}
+				}
+			}
+
+			roundScore += p
+			continue
+		} else {
+			p := gamePoints(them, me)
+			roundScore += p
+		}
+	}
+
+	return roundScore
+}
+
+func gamePoints(them, me hand) (o int) {
+	o = me.value
+	if outcomes[me.handType] == them.handType {
+		o += 6
+	}
+	if them.handType == me.handType {
+		o += 3
+	}
+	return
+}
+
+func gamePointsPart2(me string, them hand) int {
+	points := map[string]int{
+		"X": 0,
+		"Y": 3,
+		"Z": 6,
+	}
+	return points[me]
 }
--- challenges/day-2/day-2_test.go
+++ challenges/day-2/day-2_test.go
@@ -1,7 +1,24 @@
 package day2
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestDay2(t *testing.T) {
+	tests := map[string]struct {
+		input    string
+		part2    bool
+		expected int
+	}{
+		"part 1": {input: "A Y\nB X\nC Z", part2: false, expected: 15},
+		"part 2": {input: "A Y\nB X\nC Z", part2: true, expected: 12},
+	}
 
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, tc.expected, Day2(tc.input, tc.part2))
+		})
+	}
 }
