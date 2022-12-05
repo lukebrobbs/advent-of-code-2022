12ad62d70b0fa84a7d971729210a0f82d97a982e
Luke Brobbin <luke.brobbin@xdesign.com>
2022-12-05 12:27:53 +0000

    chore: day 5 part 2

--- challenges/day-5/day-5.go
+++ challenges/day-5/day-5.go
@@ -16,23 +16,20 @@ func Day5(input string, part2 bool) int {
 	instructions := createInstructions(i[1])
 
 	for _, in := range instructions {
-		for i := 1; i <= in[0]; i++ {
-			from := sta[in[1]]
-			to := sta[in[2]]
-			if len(from) > 0 {
-				sta[in[2]] = append([]string{from[0]}, to...)
-				sta[in[1]] = from[1:]
-			}
+		if part2 {
+			moveStacksPart2(in, sta)
+		} else {
+			moveStacks(in, sta)
 		}
 	}
 	str := ""
-
 	for i := 1; i <= len(sta); i++ {
 		if len(sta[i]) > 0 {
 			str += sta[i][0]
 		}
 	}
 	fmt.Print(str)
+	// add this to adhere to the interface needed to run the challenge from main.go
 	return 5
 }
 
@@ -71,4 +68,28 @@ func getIntFromString(s, split string) int {
 	ti, err := strconv.Atoi(string(u[0]))
 	utils.Check(err)
 	return ti
+}
+
+func moveStacks(in []int, sta stack) {
+	for i := 1; i <= in[0]; i++ {
+		from := sta[in[1]]
+		to := sta[in[2]]
+		if len(from) > 0 {
+			sta[in[2]] = append([]string{from[0]}, to...)
+			sta[in[1]] = from[1:]
+		}
+	}
+}
+
+func moveStacksPart2(in []int, sta stack) {
+	var sl []string
+	for i := 0; i < in[0]; i++ {
+		sl = append(sl, sta[in[1]][0])
+		if len(sta[in[1]]) > 1 {
+			sta[in[1]] = sta[in[1]][1:]
+		} else {
+			sta[in[1]] = sta[in[1]][:0]
+		}
+	}
+	sta[in[2]] = append(sl, sta[in[2]]...)
 }
