4f8e25d81c5b2c52efbedd728321414fcb6e6578
Luke Brobbin <luke.brobbin@xdesign.com>
2022-12-06 09:22:58 +0000

    chore: clean up solution

--- challenges/day-6/day-6.go
+++ challenges/day-6/day-6.go
@@ -10,18 +10,16 @@ func Day6(input string, part2 bool) int {
 	if part2 {
 		base = 13
 	}
-
-	for i := range input {
-		if i >= base {
-			for ind, ch := range input[i-base : i+1] {
-				if strings.Count(input[i-base:i+1], string(ch)) > 1 {
-					break
-				}
-				if ind == base {
-					return i + 1
-				}
+	for i := base; i <= len(input); i++ {
+		for ind, ch := range input[i-base : i+1] {
+			if strings.Count(input[i-base:i+1], string(ch)) > 1 {
+				break
+			}
+			if ind == base {
+				return i + 1
 			}
 		}
 	}
+
 	return 0
 }
